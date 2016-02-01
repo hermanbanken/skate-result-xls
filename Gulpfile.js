9020e6bef32d16b9de2ee95eebc329a6bd4a64e6
Herman Banken <hermanbanken@gmail.com>
2016-02-01 17:18:29 +0100

    Add handy gulp commands

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,8 +2,10 @@ var gulp = require("gulp");
 var sourcemaps = require("gulp-sourcemaps");
 var babel = require("gulp-babel");
 var concat = require("gulp-concat");
+var shell = require('gulp-shell');
+var watch = require('gulp-watch');
 
-gulp.task("default", function () {
+gulp.task("build", function () {
   return gulp.src(["web/angular.js", "web/ctrl/*.js"])
     .pipe(sourcemaps.init())
     .pipe(babel())
@@ -11,3 +13,15 @@ gulp.task("default", function () {
     .pipe(sourcemaps.write("."))
     .pipe(gulp.dest("web/dist"));
 });
+
+gulp.task("watch", function(cb){
+	watch('web/*.js', ['build']).on('end', cb);
+});
+
+gulp.task("server", shell.task([
+	'nodemon server.js'
+]));
+
+gulp.task("default", ['build']);
+gulp.task('dev', gulp.parallel('build', 'server', 'watch'));
+
